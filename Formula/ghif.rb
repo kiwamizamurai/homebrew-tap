d1754253f5d0a1cdee7f1c0903f54b3e20264e6d
きわみざむらい <24860100+kiwamizamurai@users.noreply.github.com>
2025-01-04 14:19:28 +0900

    Update ghif.rb

--- Formula/ghif.rb
+++ Formula/ghif.rb
@@ -7,20 +7,20 @@ class Ghif < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/kiwamizamurai/ghif/releases/download/v#{version}/ghif-x86_64-apple-darwin"
-      sha256 :no_check
+      sha256 "b53ee1f60db5f2e29e8bc325c1e43e09ee849a22d177eacd0e70e63e11f100bf"
     else
       url "https://github.com/kiwamizamurai/ghif/releases/download/v#{version}/ghif-aarch64-apple-darwin"
-      sha256 :no_check
+      sha256 "b7e50b11cc7eb205334e3d610db96cafadcc338c08c8b69fc5c5b9b9e8646902"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/kiwamizamurai/ghif/releases/download/v#{version}/ghif-x86_64-unknown-linux-gnu"
-      sha256 :no_check
+      sha256 "8f4b7cd4c2d738dd301c8c342d7371cb380cd43d0d17b70d0623f8d95aeda762"
     else
       url "https://github.com/kiwamizamurai/ghif/releases/download/v#{version}/ghif-aarch64-unknown-linux-gnu"
-      sha256 :no_check
+      sha256 "5cb33754bc031de5284c294412506ee0cfa557017cec1310edc78dcf6aeaaafd"
     end
   end
 
